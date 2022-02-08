9fedf39c387aa6c06ae5611cd8df03979a5d5b5f
aleksey <worcers@gmail.com>
2022-02-08 11:51:56 +0300

    add annotation at application class

--- src/main/java/by/overone/it/Application.java
+++ src/main/java/by/overone/it/Application.java
@@ -2,10 +2,16 @@ package by.overone.it;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("by.overone.it")
+@EntityScan("by.overone.it.entity")
+@EnableJpaRepositories("by.overone.it.repository")
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
