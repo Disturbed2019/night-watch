37906ce787cb00d477d22557e07ea2678c3feab9
aleksey <worcers@gmail.com>
2022-02-09 14:33:35 +0300

    Json файл и изменение changelog-kino.xml

--- src/main/java/by/overone/it/Application.java
+++ src/main/java/by/overone/it/Application.java
@@ -1,10 +1,14 @@
 package by.overone.it;
 
+import by.overone.it.JSON.JsonParser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -20,5 +24,12 @@ public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
+    }
+
+    @Bean
+    protected CommandLineRunner commandLineRunner(@Autowired JsonParser jsonParser) {
+        return args -> {
+            jsonParser.saveJsonFilmsIntoDatabase();
+        };
     }
 }
