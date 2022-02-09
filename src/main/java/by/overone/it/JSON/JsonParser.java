95d8a1ce9ac148cc2f88c6bc8e9d70819ba293bf
aleksey <worcers@gmail.com>
2022-02-09 14:32:08 +0300

    Json парсер для автоматического заполнения базы данных дефотными фильмами

--- src/main/java/by/overone/it/JSON/JsonParser.java
+++ src/main/java/by/overone/it/JSON/JsonParser.java
@@ -7,10 +7,12 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+@Component
 public class JsonParser {
 
     @Autowired
@@ -18,23 +20,27 @@ public class JsonParser {
 
     @SneakyThrows
     public void saveJsonFilmsIntoDatabase() {
-        BufferedReader reader = new BufferedReader(new FileReader("db.json"));
+        String path = "src/main/resources/db.json";
+        BufferedReader reader = new BufferedReader(new FileReader(path));
         JSONParser parser = new JSONParser();
         JSONObject object = (JSONObject) parser.parse(reader);
         JSONArray array = (JSONArray) object.get("films");
-        Film film = new Film();
+        Film film;
 
-        for (int i = 0; i <= array.size(); i++) {
-            object = (JSONObject) array.get(i);
-            film.setCategory(String.valueOf(object.get("category")));
-            film.setTitle(String.valueOf(object.get("title")));
-            film.setYear(String.valueOf(object.get("year")));
-            film.setBgImg(String.valueOf(object.get("bg_img")));
-            film.setDescription(String.valueOf(object.get("description")));
-            film.setPreviewImg(String.valueOf(object.get("preview_img")));
-            film.setTrailerLink(String.valueOf(object.get("trailer_link")));
-            film.setRating(String.valueOf(object.get("rating")));
-            service.saveFilm(film);
+        if (service.findFirst("horror") == null) {
+            for (Object o : array) {
+                film = new Film();
+                object = (JSONObject) o;
+                film.setCategory(String.valueOf(object.get("category")));
+                film.setTitle(String.valueOf(object.get("title")));
+                film.setYear(String.valueOf(object.get("year")));
+                film.setBgImg(String.valueOf(object.get("bg_img")));
+                film.setDescription(String.valueOf(object.get("description")));
+                film.setPreviewImg(String.valueOf(object.get("preview_img")));
+                film.setTrailerLink(String.valueOf(object.get("trailer_link")));
+                film.setRating(String.valueOf(object.get("rating")));
+                service.saveFilm(film);
+            }
         }
     }
 }
