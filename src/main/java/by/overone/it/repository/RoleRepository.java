b89c67ae05e3e3a099ee9715891772785ec9b26d
aleksey <worcers@gmail.com>
2022-02-10 20:38:46 +0300

    delete spring security classes

--- src/main/java/by/overone/it/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package by.overone.it.repository;
-
-
-import by.overone.it.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, String> {
-}
